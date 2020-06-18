922eff11ba56e24ec2aff5547157212ff652c80b
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2020-06-18 12:57:46 +0200

    Initial Commit

---
+++ SpeechRecognizerWithBuffer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechRecognizerWithBuffer
+//
+//  Created by Mostafa Abd ElFatah on 6/18/20.
+//  Copyright © 2020 asd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
